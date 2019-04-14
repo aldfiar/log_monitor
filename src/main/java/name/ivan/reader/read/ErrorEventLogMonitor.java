12d1382c60c79ddb5c87f41c42a3e854e4f5bdd2
Ivan.Shchekoldin <darthmantis@yandex.ru>
2019-04-14 22:20:10 +0300

    remove unused toString

--- src/main/java/name/ivan/reader/read/ErrorEventLogMonitor.java
+++ src/main/java/name/ivan/reader/read/ErrorEventLogMonitor.java
@@ -5,18 +5,10 @@ import name.ivan.reader.file.Reader;
 import name.ivan.reader.report.Reporter;
 import name.ivan.reader.store.Storage;
 
-import java.util.StringJoiner;
-
 public class ErrorEventLogMonitor extends EventLogMonitor {
     public ErrorEventLogMonitor(Reporter reporter, Reader reader, Storage storage) {
         super(reporter, reader, storage);
         super.eventPredicate = Conditions.isError();
     }
 
-    @Override
-    public String toString() {
-        return new StringJoiner(", ", ErrorEventLogMonitor.class.getSimpleName() + "[", "]")
-                .add("reporter=" + reporter)
-                .toString();
-    }
 }
